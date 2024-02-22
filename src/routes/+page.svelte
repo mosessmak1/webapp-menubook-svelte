1e728c0246d2d3a5d68afb6a9e709d6125a1f2a3
mosessmak1 <143071447+mosessmak1@users.noreply.github.com>
2024-02-22 08:14:29 +0000

    use grid and each

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,33 @@
-<script>
+<script lang="ts">
 	import MenuTile from '../lib/component/MenuTile.svelte';
+
+	const menus = [
+		{
+			photoUrl:
+				'https://images.immediate.co.uk/production/volatile/sites/30/2014/05/Epic-summer-salad-hub-2646e6e.jpg?resize=960%2C503',
+			name: 'Avocado Pesto Salad',
+			description:
+				'Home-grown vegetables with loads of avocado, topped with mouthwatering pesto salad'
+		},
+		{
+			photoUrl:
+				'https://www.recipetineats.com/wp-content/uploads/2023/05/Garlic-cheese-pizza_9.jpg',
+			name: 'Triple Cheese Pizza',
+			description: 'Indulge yourself in this simple yet delicious delicacy'
+		},
+		{
+			photoUrl:
+				'https://images.theconversation.com/files/525691/original/file-20230511-19-w9pz4k.jpg?ixlib=rb-1.1.0&rect=5%2C2%2C1905%2C1276&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip',
+			name: 'Seasonal Wine',
+			description: 'A great meal becomes perfect when combined with a good wine'
+		},
+		{
+			photoUrl:
+				'https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_512,h_512/https://www.adityabirlacapital.com/healthinsurance/active-together/wp-content/uploads/2019/12/How-Are-Espressos-Good-For-Health_blog-1.gif',
+			name: 'Coffee',
+			description: 'The best coffee you can have from the land of Indonesia'
+		}
+	];
 </script>
 
 <head>
@@ -27,30 +55,9 @@
 		<div class="price-note">All prices are in thousand rupiah</div>
 	</div>
 	<div class="menu-area">
-		<div class="menu-row">
-			<MenuTile
-				photoUrl="https://images.immediate.co.uk/production/volatile/sites/30/2014/05/Epic-summer-salad-hub-2646e6e.jpg?resize=960%2C503"
-				name="Avocado Pesto Salad"
-				description="Home-grown vegetables with loads of avocado, topped with mouthwatering pesto salad"
-			/>
-			<MenuTile
-				photoUrl="https://www.recipetineats.com/wp-content/uploads/2023/05/Garlic-cheese-pizza_9.jpg"
-				name="Triple Cheese Pizza"
-				description="Indulge yourself in this simple yet delicious delicacy"
-			/>
-		</div>
-		<div class="menu-row">
-			<MenuTile
-				photoUrl="https://images.theconversation.com/files/525691/original/file-20230511-19-w9pz4k.jpg?ixlib=rb-1.1.0&rect=5%2C2%2C1905%2C1276&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip"
-				name="Seasonal Wine"
-				description="A great meal becomes perfect when combined with a good wine"
-			/>
-			<MenuTile
-				photoUrl="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_512,h_512/https://www.adityabirlacapital.com/healthinsurance/active-together/wp-content/uploads/2019/12/How-Are-Espressos-Good-For-Health_blog-1.gif"
-				name="Coffee"
-				description="The best coffee you can have from the land of Indonesia"
-			/>
-		</div>
+		{#each menus as menu}
+			<MenuTile photoUrl={menu.photoUrl} name={menu.name} description={menu.description} />
+		{/each}
 	</div>
 </body>
 
@@ -108,24 +115,14 @@
 	}
 
 	div.menu-area {
-		display: flex;
-		flex-direction: column;
-		gap: 16px;
-	}
-
-	div.menu-row {
-		display: flex;
-		flex-direction: row;
+		display: grid;
+		grid-template-columns: repeat(2, minmax(0, 1fr));
 		gap: 16px;
 	}
 
 	@media (min-width: 800px) {
 		div.menu-area {
-			flex-direction: row;
-		}
-
-		div.menu-row {
-			flex: 1;
+			grid-template-columns: repeat(4, minmax(0, 1fr));
 		}
 	}
 </style>

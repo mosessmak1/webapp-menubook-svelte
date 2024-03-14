c619cf010c66fffd705a01486a9fd36bceea8206
mosessmak1 <143071447+mosessmak1@users.noreply.github.com>
2024-03-14 08:22:29 +0000

    make root layout.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -70,52 +70,30 @@
 	];
 </script>
 
-<head>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width" />
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500&display=swap"
-		rel="stylesheet"
+<div class="appbar">Kongkow Cafe</div>
+<div class="hero">
+	<img
+		src="https://cdn.themistakenman.com/wp-content/uploads/2022/08/fast-food-business.webp"
+		alt="hero"
 	/>
-</head>
-
-<body>
-	<div class="appbar">Kongkow Cafe</div>
-	<div class="hero">
-		<img
-			src="https://cdn.themistakenman.com/wp-content/uploads/2022/08/fast-food-business.webp"
-			alt="hero"
+</div>
+<div class="header-text">
+	<div class="tagline">Friends, Food, Fun</div>
+	<div class="document-title">Kongkow Cafe Menu 2023</div>
+	<div class="price-note">All prices are in thousand rupiah</div>
+</div>
+<div class="menu-area">
+	{#each menus as menu}
+		<MenuTile
+			photoUrl={menu.photoUrl}
+			name={menu.name}
+			description={menu.description}
+			prices={menu.prices}
 		/>
-	</div>
-	<div class="header-text">
-		<div class="tagline">Friends, Food, Fun</div>
-		<div class="document-title">Kongkow Cafe Menu 2023</div>
-		<div class="price-note">All prices are in thousand rupiah</div>
-	</div>
-	<div class="menu-area">
-		{#each menus as menu}
-			<MenuTile photoUrl={menu.photoUrl} name={menu.name} description={menu.description} prices={menu.prices} />
-		{/each}
-	</div>
-</body>
+	{/each}
+</div>
 
 <style>
-	/** CSS Reset */
-	*,
-	html,
-	body {
-		font-family: 'Barlow';
-		margin: 0;
-		padding: 0;
-		color: #424242;
-	}
-
-	body {
-		background-color: white;
-	}
-
 	div.appbar {
 		display: flex;
 		flex-direction: row;

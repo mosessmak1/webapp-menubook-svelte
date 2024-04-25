f087aca58c81a63e551d6961cc608f376a347673
mosessmak1 <143071447+mosessmak1@users.noreply.github.com>
2024-04-25 07:21:22 +0000

    adjust dimension of plus and minus icon

--- src/lib/icon/PlusIcon.svelte
+++ src/lib/icon/PlusIcon.svelte
@@ -1,5 +1,5 @@
 <!--https://www.svgrepo.com/svg/505461/plus-circle-->
-<svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 	<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
 	<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
 	<g id="SVGRepo_iconCarrier">

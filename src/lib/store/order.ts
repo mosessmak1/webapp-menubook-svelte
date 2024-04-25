b29d86f91eba6147b895ecf6325d217bfac504eb
mosessmak1 <143071447+mosessmak1@users.noreply.github.com>
2024-04-25 07:58:12 +0000

    make order store dynamic

--- src/lib/store/order.ts
+++ src/lib/store/order.ts
@@ -1,28 +1,7 @@
 import { derived, writable } from "svelte/store";
 
-export const saladCounter = writable(0);
-export const pizzaCounter = writable(0);
-export const wineCounter = writable(0);
-export const coffeeCounter = writable(0);
-
-// #1
+export const order = writable<{ [key: string]: number }>({});
 export const totalOrderCount = derived(
-    [saladCounter, pizzaCounter, wineCounter, coffeeCounter],
-    (c) => {
-        return c[0] + c[1] + c[2] + c[3];
-    }
+    order,
+    (o) => Object.values(o).reduce((prev, curr) => prev + curr, 0)
 );
-
-// Alternative #2 - Shorter
-// export const totalOrderCount = derived(
-//     [saladCounter, pizzaCounter, wineCounter, coffeeCounter], 
-//     ([salad, pizza, wine, coffee]) => {
-//         return salad + pizza + wine + coffee;
-//     }
-// );
-
-// Alternative #3 - Even shorter
-// export const totalOrderCount = derived(
-//     [saladCounter, pizzaCounter, wineCounter, coffeeCounter], 
-//     ([salad, pizza, wine, coffee]) => salad + pizza + wine + coffee
-// );

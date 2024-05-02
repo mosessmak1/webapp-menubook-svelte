c90826a0a87870628086e128ac5069049ef01046
mosessmak1 <143071447+mosessmak1@users.noreply.github.com>
2024-05-02 08:49:14 +0000

    counter in menu tile

--- src/lib/component/MenuTile.svelte
+++ src/lib/component/MenuTile.svelte
@@ -8,7 +8,11 @@
 	export let photoUrl: string;
 	export let prices: { id: string; label: string; price: number }[];
 
-	$: counter = $order[id] ?? 0;
+	$: counter = Object.keys($order)
+		.filter((e) => e.startsWith(id))
+		.reduce((prev, curr) => {
+			return prev + $order[curr];
+		}, 0);
 </script>
 
 <div class="menu-tile">
@@ -54,23 +58,5 @@
 		padding: 0px 16px;
 		height: 60px;
 		padding-bottom: 16px;
-	}
-
-	button {
-		padding: 8px 16px;
-		border: none;
-	}
-
-	button.add {
-		background-color: #f44336;
-		color: white;
-		margin-bottom: 8px;
-		margin-top: 16px;
-	}
-
-	button.substract {
-		border: 1px solid #f44336;
-		color: #f44336;
-		background-color: white;
 	}
 </style>

8b7c4d8901070c38be317a0a56afa1d508e0f102
mosessmak1 <143071447+mosessmak1@users.noreply.github.com>
2024-05-16 05:01:03 +0000

    move app bar to component

---
+++ src/lib/component/AppBar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { totalOrderCount } from '$lib/store/order';
+</script>
+
+<div class="appbar">Kongkow Cafe ({$totalOrderCount})</div>
+
+<style>
+	div.appbar {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		padding: 16px;
+		background-color: #f44336;
+		color: white;
+		font-size: 16px;
+		font-weight: 500;
+		position: sticky;
+		top: 0;
+		left: 0;
+		right: 0;
+	}
+</style>

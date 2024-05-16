f9036e42b910d92d7de7221dd1bbbb179857850a
mosessmak1 <143071447+mosessmak1@users.noreply.github.com>
2024-05-16 05:47:49 +0000

    add button to move back and assignment (todo)

--- src/routes/order-confirmation/+page.svelte
+++ src/routes/order-confirmation/+page.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
 	import { totalOrderValue } from '$lib/store/order';
 	import CheckIcon from '$lib/icon/CheckIcon.svelte';
+
+	const returnToMenuBook = () => {
+		// TODO: Clear cart;
+		history.back();
+	};
 </script>
+
+<!--TODO: Decorate this page-->
 
 <span>
 	<CheckIcon />
 	Terimakasih atas pesanan anda sejumlah Rp {($totalOrderValue * 1000).toLocaleString('id')}!
 </span>
 
+<button on:click={returnToMenuBook}>Kembali ke buku menu</button>
+
 <style>
 	span {
-        align-items: center;
+		align-items: center;
 		display: flex;
 		gap: 16px;
 		padding: 16px;
+	}
+
+	button {
+		margin: 16px;
 	}
 </style>

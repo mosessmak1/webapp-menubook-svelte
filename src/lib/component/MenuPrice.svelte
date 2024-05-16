ea7535677c08cb093ec3a0f409bd2a5d5b189dae
mosessmak1 <143071447+mosessmak1@users.noreply.github.com>
2024-05-16 04:58:01 +0000

    impv menu price ui

--- src/lib/component/MenuPrice.svelte
+++ src/lib/component/MenuPrice.svelte
@@ -23,24 +23,42 @@
 
 <div class="menu-price-row">
 	<span class="price-description">{label}</span>
-	<span>{price}</span>
-	<div>
+	<span class="price-value">{price}</span>
+	<div class="calculator">
 		<button on:click={substract}><MinusIcon /></button>
-		<span>{counter}</span>
+		<span class="counter">{counter}</span>
 		<button on:click={add}><PlusIcon /></button>
 	</div>
 </div>
 
 <style>
+	button {
+		background: none;
+		border: none;
+		display: flex;
+	}
+
 	div.menu-price-row {
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
 		align-items: center;
 		padding: 0 16px;
+		gap: 8px;
 	}
 
 	span.price-description {
 		font-size: 12px;
+		flex: 1;
+		text-align: start;
+	}
+
+	span.counter {
+		font-size: 12px;
+	}
+
+	div.calculator {
+		display: flex;
+		align-items: center;
 	}
 </style>

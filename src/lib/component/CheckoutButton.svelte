a067ce76e754667e123dd1aedf3d1b42794ba08b
mosessmak1 <143071447+mosessmak1@users.noreply.github.com>
2024-05-16 05:45:06 +0000

    go to order confirmation page

--- src/lib/component/CheckoutButton.svelte
+++ src/lib/component/CheckoutButton.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import CheckoutIcon from '$lib/icon/CheckoutIcon.svelte';
 	import ChevronRightIcon from '$lib/icon/ChevronRightIcon.svelte';
 	import TrashIcon from '$lib/icon/TrashIcon.svelte';
@@ -10,7 +11,7 @@
 	};
 
 	const checkout = () => {
-		// TODO:
+		goto('/order-confirmation');
 	};
 </script>
 
---
+++ src/routes/order-confirmation/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { totalOrderValue } from '$lib/store/order';
+	import CheckIcon from '$lib/icon/CheckIcon.svelte';
+</script>
+
+<span>
+	<CheckIcon />
+	Terimakasih atas pesanan anda sejumlah Rp {($totalOrderValue * 1000).toLocaleString('id')}!
+</span>
+
+<style>
+	span {
+        align-items: center;
+		display: flex;
+		gap: 16px;
+		padding: 16px;
+	}
+</style>

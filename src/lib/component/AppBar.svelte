c09743a69f501ed84d5bdf35502e3716a41ac07b
mosessmak1 <143071447+mosessmak1@users.noreply.github.com>
2024-05-16 05:28:39 +0000

    add checkout button to appbar

--- src/lib/component/AppBar.svelte
+++ src/lib/component/AppBar.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
-	import { totalOrderCount, totalOrderValue } from '$lib/store/order';
+	import { totalOrderCount } from '$lib/store/order';
+	import CheckoutButton from './CheckoutButton.svelte';
 </script>
 
 <div class="appbar">
-	Kongkow Cafe ({$totalOrderCount} items - Rp {($totalOrderValue * 1000).toLocaleString('id')})
+	<div class="spacer" />
+	<span class="title">Kongkow Cafe</span>
+	<div class="spacer">
+		{#if $totalOrderCount > 0}
+			<CheckoutButton />
+		{/if}
+	</div>
 </div>
 
 <style>
+	div.spacer {
+		display: flex;
+		flex-direction: row;
+		flex: 1;
+		justify-content: end;
+	}
+
+	span.title {
+		padding: 16px;
+	}
+
 	div.appbar {
+		gap: 16px;
 		display: flex;
 		flex-direction: row;
 		justify-content: center;
-		padding: 16px;
 		background-color: #f44336;
 		color: white;
 		font-size: 16px;
